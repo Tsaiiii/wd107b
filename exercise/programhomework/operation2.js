0a8f0bfb59d1fa03eaeaf0ab848cca2ce590f6a7
tsaiiii <may890506@gmail.com>
2019-05-31 12:16:06 +0800

    add operation2

---
+++ exercise/programhomework/operation2.js
@@ -0,0 +1,11 @@
+var i,j;
+
+for (i=1;i<10;i++) {
+    for(j=1;j<10;j++)
+    {
+        console.log("%d*%d= " ,i,j,i*j);
+        
+    }
+    console.log("%d*%d= ",i,j,i*j);
+    console.log("\n");
+  }
